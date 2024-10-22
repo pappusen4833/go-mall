3ce5eb04ceb7004790322ae3ccd47023e00157a1
0xJames <liiijz@live.com>
2024-10-22 15:30:00 +0800

    refactor(config): add WeChat configuration fields

--- conf/wechat.go
+++ conf/wechat.go
@@ -1,8 +1,10 @@
 package conf
 
 type Wechat struct {
-	AppID          string `mapstructure:"app_id" yaml:"app_id"`           //appid
-	AppSecret      string `mapstructure:"app_secret" yaml:"app_secret"`       //appsecret
-	Token          string `mapstructure:"token" yaml:"token"`            //token
+	AppID          string `mapstructure:"app_id" yaml:"app_id"`                     //appid
+	AppSecret      string `mapstructure:"app_secret" yaml:"app_secret"`             //appsecret
+	Token          string `mapstructure:"token" yaml:"token"`                       //token
 	EncodingAESKey string `mapstructure:"encoding_aes_key" yaml:"encoding_aes_key"` //EncodingAESKey
+	BaseURL        string `mapstructure:"base_url" yaml:"base_url"`                 //base url
+	RedirectURI    string `mapstructure:"redirect_uri" yaml:"redirect_uri"`         //redirect uri
 }
