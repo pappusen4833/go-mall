131b5e38a56874db5757cb857ae937e00dc4319e
0xJames <liiijz@live.com>
2025-03-08 14:55:00 +0800

    refactor(frontend): remove old pc-vue project

--- pc-vue/src/config/common.js
+++
@@ -1,8 +0,0 @@
-/**
- * 根据class获得元素
- * @param {className} class名称
- * @param {tagName} 标签名称
- */
-export function test () {
-  console.log('test')
-}
--- pc-vue/src/store/getter.js
+++
@@ -1,3 +0,0 @@
-export const loginVisible = state => state.loginVisible
-export const userInfo = state => state.userInfo
-export const cartNum = state => state.cartNum
